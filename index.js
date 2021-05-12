92f77830502864f2c5b7d5fa674f0ac461d6baab
MD AMINUL ISLAM MUKUL <saidur.shawon@gmail.com>
2021-05-13 00:42:52 +0100

    Update index.js

--- index.js
+++ index.js
@@ -366,7 +366,7 @@ io.on('connection', function (client) {
   })
 })
 
-var server_port = process.env.PORT || 3001;
+var server_port = process.env.PORT || 443;
 server.listen(server_port, function (err) {
   if (err) throw err
   console.log('Listening on port %d', server_port);
